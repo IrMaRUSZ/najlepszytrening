b3574e255de55cf0ca2812155830464a2ff51942
IrMaRUSZ <maruszewskiirek@gmail.com>
2024-12-04 00:10:37 +0100

    Usuniecie Macka

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,43 +1,42 @@
-// app/sitemap.ts
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://najlepszy-trening.pl'
-  
+
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
       url: `${baseUrl}/o-mnie`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: `${baseUrl}/trener-personalny-lodz`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: `${baseUrl}//Wspolpraca-online`,
-      lastModified: new Date(),
+      url: `${baseUrl}/wspolpraca-online`, // Poprawiony błąd w ścieżce
+      lastModified: new Date().toISOString(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: `${baseUrl}/kontakt`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: 'monthly',
       priority: 0.5,
     },
     {
       url: `${baseUrl}/blog`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: 'weekly',
       priority: 1,
     },
