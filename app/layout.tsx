30e7a2b2d73318646e9e92b6dbabf5cf233af33b
IrMaRUSZ <maruszewskiirek@gmail.com>
2024-12-07 21:37:11 +0100

    dodanie folderu icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,9 +105,6 @@ export default function RootLayout({
   return (
     <html lang="pl">
       <head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
         {generateSchemaMarkup()}
       </head>
       <body className={inter.className}>
